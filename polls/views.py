1017ba933ff9e782372a58bee9e3e3ef2ff38927
dlguswns3482 <103907181+dlguswns3482@users.noreply.github.com>
2022-08-28 00:57:54 +0900

    Django Tutorial 7 9:12

--- polls/views.py
+++ polls/views.py
@@ -47,7 +47,9 @@ def detail(request, question_id):
 
 def results(request,  question_id):
     response = "You're looking at the results of question %s."
-    return HttpResponse(response % question_id)
+    #return HttpResponse(response % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/results.html', {'question': question})
 
 def vote(request, question_id):
    # return HttpResponse("You're voting on question %s." % question_id)
@@ -62,6 +64,7 @@ def vote(request, question_id):
     else:
         selected_choice.votes += 1
         selected_choice.save()
-        return HttpResponseRedirect(reverse('polls:results', arg=(question_id, id)))
+
+        return HttpResponseRedirect(reverse('polls:results', args=(question_id,)))
 
 
