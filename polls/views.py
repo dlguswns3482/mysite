bf4e747343e77c60894e94a081881a21ea8864ba
dlguswns3482 <103907181+dlguswns3482@users.noreply.github.com>
2022-08-27 23:56:09 +0900

    Django tutoral 7

--- polls/views.py
+++ polls/views.py
@@ -2,11 +2,13 @@
 from django.http import HttpResponse
 from django.template import loader
 from django.shortcuts import render
-from django.http import Http404
+from django.http import Http404, HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render
+from django.urls import reverse
 
 
-from.models import Question
+
+from.models import Question, Choice
 # Create your views here.
 def index(request):
    #1
@@ -48,7 +50,18 @@ def results(request,  question_id):
     return HttpResponse(response % question_id)
 
 def vote(request, question_id):
-    return HttpResponse("You're voting on question %s." % question_id)
-
+   # return HttpResponse("You're voting on question %s." % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        return render(request, 'polls/detail.html',{
+            'question':question,
+            'error_message' : "You didn't select a choice."
+        })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        return HttpResponseRedirect(reverse('polls:results', arg=(question_id, id)))
 
 
