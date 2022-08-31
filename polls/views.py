aae6eb6c96de123c68d571903a890a2a78aa8675
dlguswns3482 <103907181+dlguswns3482@users.noreply.github.com>
2022-08-31 20:42:55 +0900

    Django Tutorial  8 9:00

--- polls/views.py
+++ polls/views.py
@@ -30,8 +30,8 @@ def index(request):
     context ={'latest_question_list': latest_question_list}
     return render(request, 'polls/index.html', context)
 #class IndexView(generic.ListView):
- #   template_name = 'polls/index.html'
-  #  context_object_name = 'latest_question_list'
+    #template_name = 'polls/index.html'
+    #context_object_name = 'latest_question_list'
        
     def get_queryset(self):
         return Question.objects.order_by('-pub_date')[:5]
