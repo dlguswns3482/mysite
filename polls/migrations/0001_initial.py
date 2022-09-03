83e276ae40155c00fa8253a85ca2bc9aba697b7d
dlguswns3482 <103907181+dlguswns3482@users.noreply.github.com>
2022-09-03 23:12:09 +0900

    Django Tutorial real end

--- polls/migrations/0001_initial.py
+++ polls/migrations/0001_initial.py
@@ -24,7 +24,9 @@ class Migration(migrations.Migration):
             name='Choice',
             fields=[
                 ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('choice_text', models.CharField(max_length=200)),
+                ('choice_text', models.CharField(max_length=500j
+                                                 
+                                                 )),
                 ('votes', models.IntegerField(default=0)),
                 ('question', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to='polls.question')),
             ],
--- polls/tests.py
+++ polls/tests.py
@@ -10,4 +10,4 @@ class QuestionModelTests(TestCase):
         time=timezone.now() + datetime.timedelta(days=30)
         future_question=Question(date=time)
         
-        self.assertIs(future_question.was_published_recently(), False)
+        self.assertIS(future_question.was_published_recently(), True)
